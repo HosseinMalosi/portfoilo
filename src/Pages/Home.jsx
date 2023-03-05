1a2f29ff10c3f3e9cd3acd4fefdb2d3a60fd09b7
hossein <hossmalos@gmail.com>
2023-03-05 18:35:41 +0330

    1 part

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,21 +1,55 @@
 import React from "react";
+import { Instagram, Telegram, Linkedin, Whatsapp } from "react-bootstrap-icons";
 import GlassyCard from "../Components/GlassyCard";
-import x from "../asset/portrait.jpg"
+import x from "../asset/portrait.jpg";
 
 const Home = () => {
   return (
-    <div className="flex flex-col-reverse md:flex-row bg-[hsl(0,0%,9%)] h-screen max-h-full px-4 py-2 gap-2 text-white">
+    <div className="flex flex-col-reverse md:flex-row bg-[hsl(0,0%,9%)] h-screen max-h-full px-4 py-2 gap-2 text-white overflow-hidden">
       <div className=" basis-2/3 md:basis-3/5">
         <GlassyCard className="h-full bg-[rgb(34,95,80)] rounded-lg">
           dawda
         </GlassyCard>
       </div>
       <div className="flex flex-col basis-1/3 md:basis-2/5 md:gap-3 gap-1">
-        <GlassyCard className="flex lg:md:basis-2/3 md:flex-col w-full h-3/4 md:h-4/6 bg-[rgb(53,54,129)] rounded-lg">
-         <div className="flex justify-center basis-2/4"><img  className="md:h-2/3 md:w-40 lg:h-2/3 lg:w-52 lg:scale-75 bg-[rgb(59,61,145)] shadow-2xl p-2 rounded-[100%]" src={x} alt="" /></div> 
-         <div className="basis-2/4">adw</div>
+        <GlassyCard className="flex md:flex-col w-full min-h-[10.9rem] max-h-[10.9rem] md:h-3/6 bg-[rgb(53,54,129)] rounded-lg">
+          <div className="flex justify-between h-full p-1">
+            <div className="flex items-center pl-10 w-1/3 ">
+              <div className=" grid grid-cols-2 grid-rows-2 gap-6">
+                <a
+                  className=" bg-slate-300 text-fuchsia-700 p-1 rounded text-3xl hover:shadow-lg"
+                  href=""
+                >
+                  <Instagram />
+                </a>
+                <a
+                  className="bg-slate-300 text-sky-600 p-1 rounded text-3xl hover:shadow-lg"
+                  href=""
+                >
+                  <Telegram />
+                </a>
+                <a
+                  className="bg-slate-300 text-blue-800 p-1 rounded text-3xl hover:shadow-lg"
+                  href=""
+                >
+                  <Linkedin />
+                </a>
+                <a
+                  className="bg-slate-300 text-green-600 fill-black p-1 rounded text-3xl hover:shadow-lg"
+                  href=""
+                >
+                  <Whatsapp/>
+                </a>
+              </div>
+            </div>
+            <img
+              className="lg:h-40 md:h-40 md:w-40   bg-[rgb(59,61,145)] shadow-2xl p-2 rounded-[100%]"
+              src={x}
+              alt=""
+            />
+          </div>
         </GlassyCard>
-        <GlassyCard className="hidden lg:md:basis-1/3 md:block h-1/2 md:h-2/4 bg-[rgb(53,54,129)] rounded-lg">
+        <GlassyCard className="hidden lg:basis-1/3 md:basis- md:h-4/4 md:block h-1/2  bg-[rgb(53,54,129)] rounded-lg">
           dawda
         </GlassyCard>
       </div>
